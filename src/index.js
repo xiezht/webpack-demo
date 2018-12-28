18a0ab12280e4ce92cba4e3fbef8ed7ed497ccbf
xiezhentian <xiezhentian@xiaomi.com>
2018-12-28 21:14:09 +0800

    learn how to manage output

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
@@ -11,8 +13,11 @@ function component() {
   // icon.src = Icon
   // icon.width = 100
   // icon.height = 100
-
   // element.appendChild(icon)
+
+  btn.innerHTML = 'Click me and check the console'
+  btn.onclick = printMe
+  element.appendChild(btn)
 
   return element
 }
