b6be333556334a33e5451f8454aba1d88dbb56b5
xiezhentian <xiezhentian@xiaomi.com>
2018-12-28 14:14:56 +0800

    try asset management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import Icon from './rocket.png'
 import _ from 'lodash'
+import './style.css'
+
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('div')
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
 
-  return element;
+  // var icon = new Image()
+  // icon.src = Icon
+  // icon.width = 100
+  // icon.height = 100
+
+  // element.appendChild(icon)
+
+  return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,26 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader'
+      ]
+    }, {
+      test: /\.(ttf|woff|woff2|eot|otf)$/,
+      use: [
+        'file-loader'
+      ]
+    }]
   }
 }
